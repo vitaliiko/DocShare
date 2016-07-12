c7066d72d7b4c1cd7848b2a71ad644db072df372
vitaliykobrin <vitaliy.kobrin@gmail.com>
2016-07-12 19:38:09 +0300

    removed unused constructors in custom exceptions

--- src/main/java/com/geekhub/exceptions/ValidateUserInformationException.java
+++ src/main/java/com/geekhub/exceptions/ValidateUserInformationException.java
@@ -9,34 +9,13 @@ public class ValidateUserInformationException extends Exception {
 
     private UserDto userDto;
 
-    public ValidateUserInformationException() {}
-
     public ValidateUserInformationException(String message) {
         super(message);
-    }
-
-    public ValidateUserInformationException(String message, RegistrationInfoDto registrationInfoDto) {
-        super(message);
-        this.registrationInfoDto = registrationInfoDto;
     }
 
     public ValidateUserInformationException(String message, UserDto userDto) {
         super(message);
         this.userDto = userDto;
-    }
-
-    public ValidateUserInformationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public ValidateUserInformationException(Throwable cause) {
-        super(cause);
-    }
-
-    public ValidateUserInformationException(String message, Throwable cause,
-                                            boolean enableSuppression, boolean writableStackTrace) {
-
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 
     public RegistrationInfoDto getRegistrationInfoDto() {
