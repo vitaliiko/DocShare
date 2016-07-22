b7028873159746abeae13368f3beb16f7e442fe7
vitaliykobrin <vitaliy.kobrin@gmail.com>
2016-07-22 16:01:52 +0300

    add AccessPredicates, add predicates for FilesResource

---
+++ src/main/java/com/geekhub/security/AccessPredicates.java
@@ -0,0 +1,71 @@
+package com.geekhub.security;
+
+import com.geekhub.entities.User;
+import com.geekhub.entities.UserDirectory;
+import com.geekhub.entities.UserDocument;
+import com.geekhub.entities.enums.DocumentStatus;
+import com.geekhub.entities.enums.FileRelationType;
+
+import java.util.List;
+import java.util.function.BiPredicate;
+
+public class AccessPredicates {
+
+    public static final BiPredicate<User, UserDocument> DOCUMENT_OWNER = (u, d) -> {
+        FileRelationType relationType = FileAccessService.getUserToDocumentRelationType(d, u);
+        return relationType != null
+                && relationType == FileRelationType.OWN
+                && d.getDocumentStatus() == DocumentStatus.ACTUAL;
+    };
+
+    public static final BiPredicate<User, UserDocument> REMOVED_DOCUMENT_OWNER = (u, d) -> {
+        FileRelationType relationType = FileAccessService.getUserToDocumentRelationType(d, u);
+        return relationType != null
+                && relationType == FileRelationType.OWN
+                && d.getDocumentStatus() == DocumentStatus.REMOVED;
+    };
+
+    public static final BiPredicate<User, UserDocument> DOCUMENT_READER = (u, d) -> {
+        FileRelationType relationType = FileAccessService.getUserToDocumentRelationType(d, u);
+        return d.getDocumentStatus() == DocumentStatus.ACTUAL
+                && (relationType != null || FileAccessService.isInReaderOrEditorGroups(u, d));
+    };
+
+    public static final BiPredicate<User, UserDocument> DOCUMENT_EDITOR = (u, d) -> {
+        FileRelationType relationType = FileAccessService.getUserToDocumentRelationType(d, u);
+        return relationType != null
+                && d.getDocumentStatus() == DocumentStatus.ACTUAL
+                && (relationType != FileRelationType.READ || FileAccessService.isInEditorGroups(u, d));
+    };
+
+    public static final BiPredicate<User, UserDirectory> DIRECTORY_OWNER = (u, d) -> {
+        FileRelationType relationType = FileAccessService.getUserToDirectoryRelationType(d, u);
+        return relationType != null
+                && relationType == FileRelationType.OWN
+                && d.getDocumentStatus() == DocumentStatus.ACTUAL;
+    };
+
+    public static final BiPredicate<User, UserDirectory> DIRECTORY_READER = (u, d) -> {
+        FileRelationType relationType = FileAccessService.getUserToDirectoryRelationType(d, u);
+        return d.getDocumentStatus() == DocumentStatus.ACTUAL
+                && (relationType != null || FileAccessService.isInReaderOrEditorGroups(u, d));
+    };
+
+    public static final BiPredicate<User, UserDirectory> DIRECTORY_EDITOR = (u, d) -> {
+        FileRelationType relationType = FileAccessService.getUserToDirectoryRelationType(d, u);
+        return d.getDocumentStatus() == DocumentStatus.ACTUAL
+                && (relationType != FileRelationType.READ || FileAccessService.isInEditorGroups(u, d));
+    };
+
+    public static final BiPredicate<User, List<UserDirectory>> DIRECTORIES_OWNER = (u, directories) -> {
+        List<FileRelationType> relationTypes = FileAccessService.getUserToDirectoriesRelationTypes(directories, u);
+        return directories.stream().map(UserDirectory::getDocumentStatus).allMatch(s -> s == DocumentStatus.ACTUAL)
+                && relationTypes.stream().allMatch(r -> r == FileRelationType.OWN);
+    };
+
+    public static final BiPredicate<User, List<UserDocument>> DOCUMENTS_OWNER = (u, documents) -> {
+        List<FileRelationType> relationTypes = FileAccessService.getUserToDocumentsRelationTypes(documents, u);
+        return documents.stream().map(UserDocument::getDocumentStatus).allMatch(s -> s == DocumentStatus.ACTUAL)
+                && relationTypes.stream().allMatch(r -> r == FileRelationType.OWN);
+    };
+}
--- src/main/java/com/geekhub/security/FileAccessService.java
+++ src/main/java/com/geekhub/security/FileAccessService.java
@@ -1,13 +1,14 @@
 package com.geekhub.security;
 
 import com.geekhub.entities.*;
-import com.geekhub.entities.enums.DocumentStatus;
 import com.geekhub.entities.enums.FileRelationType;
 import com.geekhub.services.*;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
 import java.util.function.BiPredicate;
 
@@ -39,93 +40,54 @@ public class FileAccessService {
         staticFriendGroupToDirectoryRelationService = friendGroupToDirectoryRelationService;
     }
 
-
-    public static final BiPredicate<User, UserDocument> DOCUMENT_OWNER = (u, d) -> {
-        FileRelationType relationType = getUserToDocumentRelationType(d.getId(), u.getId());
-        return relationType != null
-                && relationType == FileRelationType.OWN
-                && d.getDocumentStatus() == DocumentStatus.ACTUAL;
-    };
-
-    public static final BiPredicate<User, UserDocument> REMOVED_DOCUMENT_OWNER = (u, d) -> {
-        FileRelationType relationType = getUserToDocumentRelationType(d.getId(), u.getId());
-        return relationType != null
-                && relationType == FileRelationType.OWN
-                && d.getDocumentStatus() == DocumentStatus.REMOVED;
-    };
-
-    public static final BiPredicate<User, UserDocument> DOCUMENT_READER = (u, d) -> {
-        FileRelationType relationType = getUserToDocumentRelationType(d.getId(), u.getId());
-        return d.getDocumentStatus() == DocumentStatus.ACTUAL
-                && (relationType != null || isInReaderOrEditorGroups(u, d));
-    };
-
-    public static final BiPredicate<User, UserDocument> DOCUMENT_EDITOR = (u, d) -> {
-        FileRelationType relationType = getUserToDocumentRelationType(d.getId(), u.getId());
-        return relationType != null
-                && d.getDocumentStatus() == DocumentStatus.ACTUAL
-                && (relationType != FileRelationType.READ || isInEditorGroups(u, d));
-    };
-
-    public static final BiPredicate<User, UserDirectory> DIRECTORY_OWNER = (u, d) -> {
-        FileRelationType relationType = getUserToDirectoryRelationType(d.getId(), u.getId());
-        return relationType != null
-                && relationType == FileRelationType.OWN
-                && d.getDocumentStatus() == DocumentStatus.ACTUAL;
-    };
-
-    public static final BiPredicate<User, UserDirectory> DIRECTORY_READER = (u, d) -> {
-        FileRelationType relationType = getUserToDirectoryRelationType(d.getId(), u.getId());
-        return d.getDocumentStatus() == DocumentStatus.ACTUAL
-                && (relationType != null || isInReaderOrEditorGroups(u, d));
-    };
-
-    public static final BiPredicate<User, UserDirectory> DIRECTORY_EDITOR = (u, d) -> {
-        FileRelationType relationType = getUserToDirectoryRelationType(d.getId(), u.getId());
-        return d.getDocumentStatus() == DocumentStatus.ACTUAL
-                && (relationType != FileRelationType.READ || isInEditorGroups(u, d));
-    };
-
-
     public <T> boolean permitAccess(T document, User user, BiPredicate<User, T> predicate) {
         return predicate.test(user, document);
     }
 
-    private static FileRelationType getUserToDocumentRelationType(Long documentId, Long userId) {
-        UserToDocumentRelation relation = staticUserToDocumentRelationService.getByDocumentIdAndUserId(documentId, userId);
+
+    protected static FileRelationType getUserToDocumentRelationType(UserDocument document, User user) {
+        UserToDocumentRelation relation = staticUserToDocumentRelationService.getByDocumentAndUser(document, user);
         if (relation == null) {
             return null;
         }
         return relation.getFileRelationType();
     }
 
-    private static FileRelationType getUserToDirectoryRelationType(Long documentId, Long userId) {
-        UserToDirectoryRelation relation = staticUserToDirectoryRelationService.getByDirectoryIdAndUserId(documentId, userId);
+    protected static FileRelationType getUserToDirectoryRelationType(UserDirectory directory, User user) {
+        UserToDirectoryRelation relation = staticUserToDirectoryRelationService.getByDirectoryAndUser(directory, user);
         if (relation == null) {
             return null;
         }
         return relation.getFileRelationType();
     }
 
-    private static boolean isInReaderOrEditorGroups(User user, UserDocument document) {
+    protected static List<FileRelationType> getUserToDirectoriesRelationTypes(List<UserDirectory> directories, User user) {
+        return staticUserToDirectoryRelationService.getAllRelationsByDirectoriesAndUser(directories, user);
+    }
+
+    protected static List<FileRelationType> getUserToDocumentsRelationTypes(List<UserDocument> documents, User user) {
+        return staticUserToDocumentRelationService.getAllRelationsByDocumentsAndUser(documents, user);
+    }
+
+    protected static boolean isInReaderOrEditorGroups(User user, UserDocument document) {
         List<FileRelationType> relations = staticFriendGroupToDocumentRelationService
                 .getAllRelationsByDocumentIdAndUser(document.getId(), user);
         return relations.size() > 0;
     }
 
-    private static boolean isInReaderOrEditorGroups(User user, UserDirectory directory) {
+    protected static boolean isInReaderOrEditorGroups(User user, UserDirectory directory) {
         List<FileRelationType> relations = staticFriendGroupToDirectoryRelationService
                 .getAllRelationsByDirectoryIdAndUser(directory.getId(), user);
         return relations.size() > 0;
     }
 
-    private static boolean isInEditorGroups(User user, UserDocument document) {
+    protected static boolean isInEditorGroups(User user, UserDocument document) {
         List<FileRelationType> relations = staticFriendGroupToDocumentRelationService
                 .getAllRelationsByDocumentIdAndUser(document.getId(), user);
         return relations.contains(FileRelationType.EDIT);
     }
 
-    private static boolean isInEditorGroups(User user, UserDirectory directory) {
+    protected static boolean isInEditorGroups(User user, UserDirectory directory) {
         List<FileRelationType> relations = staticFriendGroupToDirectoryRelationService
                 .getAllRelationsByDirectoryIdAndUser(directory.getId(), user);
         return relations.contains(FileRelationType.EDIT);
